4eaafd881046ae9247727650822ddb6037fff3bf
Sam Heijnen <50697421+Heijneken@users.noreply.github.com>
2019-05-16 11:52:27 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+// 1 selector
+// 2 function
+// 3 parameters
+// 4 click function
+//
+
+
+$("#button").click( function(){
+    $("#button").css("color", "#FFF");
+    // $("body").fadeOut(10000);
+    $("#button").animate({"margin-right": "400px" }, 5000);
+} );
+
+
+
